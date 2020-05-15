1e8f7145ac8b9e2f410a2144aaa0e186983205ab
AlfredYuisGood <64243027+AlfredYuisGood@users.noreply.github.com>
2020-05-15 10:54:46 +1000

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Catherine1',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Catherine2',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Catherine3',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Catherine4',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Catherine5',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Catherine6',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Catherine7',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Catherine8',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Catherine9',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Catherine10',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
